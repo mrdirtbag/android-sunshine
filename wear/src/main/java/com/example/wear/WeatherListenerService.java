8d491fd08358632d91355af76c37cc4d91f8f8c8
dirtbag <pmcgoldrick@gmail.com>
2017-02-17 05:05:16 -0800

    wear app minimally working, now adding data between phone and wearable

---
+++ wear/src/main/java/com/example/wear/WeatherListenerService.java
@@ -0,0 +1,37 @@
+package com.example.wear;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Watch listening for updates from phone app on weather data.
+ * Want (3) pieces of data:
+ *   High
+ *   Low
+ *   image representing forcast
+ */
+public class WeatherListenerService extends WearableListenerService {
+
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for(DataEvent dataEvent : dataEvents) {
+            if(dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if(path.equals("/weather")) {
+                    MyWatchFace.HIGH = dataMap.getInt("high");
+                    MyWatchFace.LOW  = dataMap.getInt("low");
+                    // TODO: get asset
+                }
+            }
+        }
+    }
+}
